1f76d4f898909837939c5d42be54a8751b29d072
zeinab-Ahmed <44482247+zozoahmed@users.noreply.github.com>
2022-06-05 16:45:46 +0200

    Add files via upload

---
+++ indextask1.php
@@ -0,0 +1,27 @@
+<?php
+ 
+$text ="eeeee";
+$name ="ffff";
+
+echo $text.$name;
+
+echo "======================";
+/*task1*/
+$x = 10; 
+$y = 5;
+ 
+ $x = $x + $y;  
+$y = $x - $y;  
+$x = $x - $y;  
+ 
+echo "After Swapping: x = ",$x, "x= ", "y = ", $y;
+
+
+echo "======================";
+
+/*task2*/
+$text1 = "Good Morning.";
+$text2 = "Have a nice day!";
+echo $text1.$text2;
+
+?>
